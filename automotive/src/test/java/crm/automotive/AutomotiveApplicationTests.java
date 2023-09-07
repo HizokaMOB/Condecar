2b1f19df28adfb601f7943a3f52887c9308254b2
DanielHernandez <daniel_hernandez82202@elpoli.edu.co>
2023-09-06 22:23:47 -0500

    build(Backend):
    add the necessary dependencies to correctly use the framework

---
+++ automotive/src/test/java/crm/automotive/AutomotiveApplicationTests.java
@@ -0,0 +1,13 @@
+package crm.automotive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutomotiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
